9c33feb6222cca89286ffb2db34b618a9724d7dd
kuzeykose <kosekuzey97@gmail.com>
2022-01-07 23:29:49 +0300

    Button component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Button from './compenents/button'
+
 function App() {
+
+  const myFunction = () => {
+    console.log('aa');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button
+        style={{background: 'red'}}
+        onClick={myFunction}
+        traditionalCSS='defaultButton'
+      >
+        Save
+      </Button>
     </div>
   );
 }
