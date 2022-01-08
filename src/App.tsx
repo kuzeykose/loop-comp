b90848b0f920487eecfbecfd585be1608f5c945d
kuzeykose <kosekuzey97@gmail.com>
2022-01-09 00:14:05 +0300

    Card component added v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
 import Button from './compenents/button'
+import Card from './compenents/card'
 
 function App() {
   return (
-    <Button>
-      Submit
-    </Button>
+    <>
+      <Card
+        title='My blog post'
+        cover={'https://source.unsplash.com/random/445x150'}
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Autem repudiandae ex, consectetur sit explicabo sequi magnam aliquid eveniet
+          quia error a, ab fugit vel debitis fuga repellendus rem quo facilis.
+        </p>
+      </Card>
+      <Button>Submit</Button>
+    </>
   );
 }
 
